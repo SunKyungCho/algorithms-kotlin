dc93c5c5ef679ee36b8f1380faedddea2c8c71e6
sunkyung <sunkyungch@gmail.com>
2025-05-07 22:20:17 +0900

    first commit

---
+++ src/main/kotlin/leetCode/easy/268_MissingNumber.kt
@@ -0,0 +1,19 @@
+package me.study.leetCode.easy
+
+class MissingNumber {
+
+
+    fun missingNumber(nums: IntArray): Int {
+
+        val n = nums.size
+        val total = n * (n + 1) / 2
+        val sum = nums.sum()
+
+        return  total - sum
+    }
+}
+
+
+fun main() {
+    print("hello")
+}
